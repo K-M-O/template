1581b25c66da59741b8e7a19ec7b925674600d6b
KMO <itigamer123@gmail.com>
2020-10-03 13:02:32 +0200

    Initial Author Router

--- routes/authors.js
+++ routes/authors.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const Author = require('../models/author')
 const router = express.Router();
 // all authors
-router.get('/', (req, res)=>{
-    res.render('authors/index');
+router.get('/', async(req, res)=>{
+    let searchOptions = {};
+    if (req.query.name !== null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i');
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', {
+            authors : authors,
+            searchOptions : req.query
+        });
+    } catch (error){
+        res.redirect('/');
+        console.error(error);;
+    }
 });
 // new author
 router.get('/new', (req,res) =>{
-    res.render('authors/new');
+    res.render('authors/new', { author : new Author()});
 });
 // create author
-router.post('/', (req, res)=>{
-    res.send('create');
+router.post('/', async(req, res)=>{
+    const author = new Author({
+        name : req.body.name
+    });
+    try {
+        const newAuthor = await author.save();
+        //res.redirect(`authors/${newAuthor.id}`);
+        res.redirect(`authors`);
+    } catch  {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        });
+    }
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 const authorsRouter = require('./routes/authors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser : true})
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
